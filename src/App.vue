4f445bcdf3468f592d34a8b148f9a1198502831d
edwardrand <52064142+tanmay-kali@users.noreply.github.com>
2022-04-02 19:34:57 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+import AddTasks from "./components/AddTasks.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+    AddTasks,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id) {
+      if (confirm("Are you sure")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Samuel Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "Study Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+    ];
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    ></Header>
+    <div v-show="showAddTask">
+      <AddTasks @add-task="addTask"></AddTasks>
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    ></Tasks>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    ></Button>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
